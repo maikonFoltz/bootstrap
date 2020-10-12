93767fbe2cdc41d525defd7bac906927273f089f
maikonFoltz <maikonjfoltz@gmail.com>
2020-10-12 18:56:19 -0300

    ok

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos/bootstrap/css/bootstrap.min.css">
+    
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="container">
+        <h1>Hello Word !!</h1>
+        
+    </div>
+    <script type="text/javascript" src="estilos/bootstrap/js/jquery.min.js"></script>
+    <script type="text/javascript" src="estilos/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
